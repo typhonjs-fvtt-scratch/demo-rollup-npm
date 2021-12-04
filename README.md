5678f1c5391e94a8dcc5d72a32df871a00a4a499
typhonrt <support@typhonjs.io>
2021-12-03 21:29:26 -0800

    Added CommonJS bundling example.

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ This is in response to a question on the League of Extraordinary Foundry Develop
 
 This repo shows how to bundle an ESM Node module / [tippy.js](https://www.npmjs.com/package/tippy.js) for use in the
 browser. `tippy.js` depends on another Node module `popper.js` which is bundled together by the basic Rollup 
-configuration provided. `./src/tippy.js` simply exports the default export of the `tippy.js` module and Rollup goes to 
-work including all of the resources in `./dist/tippy.js`. You can now copy this file into your own build process and
-include this file in your project. Take note that `tippy.js` also has a CSS file which you will need to add to your 
-FVTT manifest in `styles`. I suppose a better solution though more involved is to use PostCSS and namespace the CSS for 
-tippy to your module / system. This is not covered in this basic solution. 
+configuration provided. `@rollup/plugin-virtual` is used to virtually export the entry points to the NPM modules being 
+bundled. Take note that `tippy.js` also has a CSS file which you will need to add to your FVTT manifest in `styles`.  
 
-I have checked in the `./dist/tippy.js` file and you can of course just grab it and use it, but to build it:
+Another example is bundling a CommonJS module for usage as ESM. I randomly picked out `lz4js` which is a CommonJS
+NPM package. The only difference in the Rollup config is using `@rollup/plugin-commonjs` which comes after the
+Node resolve plugin. 
 
+To run the bundling process:
 - npm install
 - npm run bundle
--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
+import commonjs      from '@rollup/plugin-commonjs';     // Use this plugin to resolve CommonJS modules.
 import resolve       from '@rollup/plugin-node-resolve'; // This resolves NPM modules from node_modules.
 import sourcemaps    from 'rollup-plugin-sourcemaps';    // This will resolve sourcemaps from node_modules.
 import { terser }    from 'rollup-plugin-terser';        // Terser is used for minification / mangling
 
 // Import config files for Terser; refer to respective documentation for more information.
 import terserConfig  from './terser.config';
+import virtual from "@rollup/plugin-virtual";
 
 // Produce sourcemaps or not.
 const s_SOURCEMAP = true;
 
 // Adds Terser to the output plugins for server bundle if true.
-const s_MINIFY = true;
+const s_MINIFY = false;
 
 export default () =>
 {
@@ -21,8 +23,8 @@ export default () =>
       outputPlugins.push(terser(terserConfig));
    }
 
-   return [{   // This bundle is for the Node distribution.
-         input: ['src/tippy.js'],
+   return [{   // `tippy.js` is already ESM, so just need Node resolve.
+         input: 'pack',
          output: [{
             file: `dist/tippy.js`,
             format: 'es',
@@ -31,7 +33,28 @@ export default () =>
             sourcemap: s_SOURCEMAP,
          }],
          plugins: [
+            virtual({
+               pack: `export { default } from 'tippy.js';`
+            }),
             resolve(),
+            sourcemaps()
+         ]
+      },
+      {   // This bundle shows a config for a CommonJS module; basically add `commonjs` plugin after Node resolve.
+         input: 'pack',
+         output: [{
+            file: `dist/lz4js.js`,
+            format: 'es',
+            plugins: outputPlugins,
+            preferConst: true,
+            sourcemap: s_SOURCEMAP,
+         }],
+         plugins: [
+            virtual({
+               pack: `export { default } from 'lz4js';`
+            }),
+            resolve(),
+            commonjs(),
             sourcemaps()
          ]
       }
